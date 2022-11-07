5ccb3cc40b349146f7ec1906bbe4a82e0c93f9ca
in-ch <sxin2949@naver.com>
2022-11-07 17:58:25 +0900

    chore(:wrench:): internal package math-helpers 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require("next-transpile-modules")([
+  // Add "math-helpers" to this array:
+  "math-helpers",
+]);
+
+module.exports = withTM({
   reactStrictMode: true,
   experimental: {
     transpilePackages: ["ui"],
   },
-};
+});
